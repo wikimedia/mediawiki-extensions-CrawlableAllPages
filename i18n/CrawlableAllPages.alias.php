55b1017cef5e522e97cfbc54585df3bd708f3f5b
Mark A. Hershberger <mah@nichework.com>
2019-03-19 20:34:06 -0400

    initial commit

---
+++ i18n/CrawlableAllPages.alias.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Aliases for crawlableallpages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English
+ * @author Mark A. Hershberger
+ */
+$specialPageAliases['en'] = [
+	'CrawlableAllPages' => [ 'CrawlableAllPages', 'All pages (crawlable)' ],
+];
